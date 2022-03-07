2e2f57dcc53c9fdfe1457f7966da3980e339598c
balamuru <balamuru@gmail.com>
2022-03-07 04:16:04 -0600

    added  more goroutine/sync examples

--- app.go
+++ app.go
@@ -8,11 +8,15 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
+// https://notes.shichao.io/gopl/ch8/#channels
 func main() {
 
 	// sequential()
@@ -20,8 +24,91 @@ func main() {
 	// goroutines_naive()
 	// goroutines_channels()
 	// goroutines_spinner()
-	goroutines_timeserver()
+	// goroutines_timeserver()
+	// waitGroups()
+	// syncDemoUsingAtomic()
+	syncDemoUsingMutex()
 
+}
+
+func syncDemoUsingMutex() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	f := func(waitGroup *sync.WaitGroup, mutex *sync.Mutex, num *int64) {
+		defer waitGroup.Done() //decrement
+
+		for i := 0; i < 1000; i++ {
+
+			func() {
+				defer mutex.Unlock() //ensure unlock
+
+				mutex.Lock() //lock
+
+				*num++
+			}()
+		}
+	}
+
+	var waitGroup sync.WaitGroup
+	var mutex sync.Mutex
+
+	var num int64 = 0
+	for i := 0; i < 1000; i++ {
+		waitGroup.Add(1)               //increment
+		go f(&waitGroup, &mutex, &num) //pass in ref
+	}
+
+	waitGroup.Wait() //wait till 0
+
+	println(num)
+
+	println("done")
+}
+
+func syncDemoUsingAtomic() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	f := func(waitGroup *sync.WaitGroup, num *int64) {
+		defer waitGroup.Done() //decrement
+
+		for i := 0; i < 1000; i++ {
+			atomic.AddInt64(num, 1)
+			// *num++
+		}
+	}
+
+	var waitGroup sync.WaitGroup
+
+	var num int64 = 0
+	for i := 0; i < 1000; i++ {
+		waitGroup.Add(1)       //increment
+		go f(&waitGroup, &num) //pass in ref
+	}
+
+	waitGroup.Wait() //wait till 0
+
+	println(num)
+
+	println("done")
+}
+
+func waitGroups() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	f := func(waitGroup *sync.WaitGroup, i int) {
+		defer waitGroup.Done() //decrement
+		println(i)
+	}
+
+	var waitGroup sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		waitGroup.Add(1) //increment
+		// go f(&waitGroup, i) - bad - don't pass in copy - will deadlock
+		go f(&waitGroup, i) //pass in ref
+	}
+
+	waitGroup.Wait() //wait till 0
+
+	println("done")
 }
 
 //client can connect via nc eg nc localhost 8000
@@ -33,7 +120,7 @@ func goroutines_timeserver() {
 
 	for {
 		conn, err := listener.Accept()
-		
+
 		if err != nil {
 			log.Print(err)
 			continue
@@ -45,24 +132,22 @@ func goroutines_timeserver() {
 }
 
 func handleEchoConn(c net.Conn) {
-    input := bufio.NewScanner(c)
-    for input.Scan() {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
 		//a nested goroutine to make each echo async
-        go echo(c, input.Text(), 1*time.Second)
-    }
-    // NOTE: ignoring potential errors from input.Err()
-    c.Close()
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
 }
 
 func echo(c net.Conn, shout string, delay time.Duration) {
-    fmt.Fprintln(c, "\t", strings.ToUpper(shout))
-    time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
 	fmt.Fprintln(c, "\t\t", strings.ToUpper(shout))
-    time.Sleep(delay)
+	time.Sleep(delay)
 	fmt.Fprintln(c, "\t\t\t", strings.ToUpper(shout))
 }
-
-
 
 func handleConn(c net.Conn) {
 	defer c.Close()
@@ -92,17 +177,18 @@ func spinner(delay time.Duration) {
 
 func goroutines_channels() {
 	urlSizeDetailsChannel := make(chan string)
+	defer close(urlSizeDetailsChannel)
 	go responseSize2("https://www.example.com", urlSizeDetailsChannel)
 	go responseSize2("https://www.golang.org", urlSizeDetailsChannel)
 	go responseSize2("https://www.golang.org/doc", urlSizeDetailsChannel)
-	println(<-urlSizeDetailsChannel)
-	println(<-urlSizeDetailsChannel)
-	println(<-urlSizeDetailsChannel)
+	println(<-urlSizeDetailsChannel) //channel sends to println
+	println(<-urlSizeDetailsChannel) //channel sends to println
+	println(<-urlSizeDetailsChannel) //channel sends to println
 
 }
 
 func responseSize2(url string, urlSizeDetailsChannel chan string) {
-	urlSizeDetailsChannel <- responseSizeHelper(url)
+	urlSizeDetailsChannel <- responseSizeHelper(url) //channel receives
 }
 
 func goroutines_naive() {
--- app_test.go
+++ app_test.go
@@ -2,12 +2,11 @@ package main
 
 import "testing"
 
-
 func TestHelloName(t *testing.T) {
-want := "https://www.example.com : 1256"
-got := responseSizeHelper("https://www.example.com")
-println(got)
-    if want != got  {
-        t.Fatalf("wanted %s but got %s", want, got)
-    }
+	want := "https://www.example.com : 1256"
+	got := responseSizeHelper("https://www.example.com")
+	println(got)
+	if want != got {
+		t.Fatalf("wanted %s but got %s", want, got)
+	}
 }
